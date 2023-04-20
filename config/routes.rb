4857eb0d5679d3d3baf29f0304679350be42aa96
+ <+>
2023-04-20 09:49:19 -0400

    updating routing for all models

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  resources :users, only:[:index]
+  resources :events, only:[:index]
+  resources :reviews
 end
