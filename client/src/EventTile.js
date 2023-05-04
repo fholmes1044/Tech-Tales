274356ca9b47a694611dc051b4125e27af502a13
+ <+>
2023-05-04 12:00:44 -0400

    all events tab also shows all reviews

--- client/src/EventTile.js
+++ client/src/EventTile.js
@@ -4,15 +4,26 @@ function EventTile({event}){
    const {title, event_description, price, reviews} = event
    const [allReviews, setAllReviews] = useState([])
 
-//    const allReviewsMap = reviews.map((review) =>{
-//     setAllReviews(review)
-//    })
+   const allReviewsMap = reviews.map((review) =>{
+    return(
+        <li key={review.id}>{review.summary}</li>
+    )
+   })
+//     const allReviewsMap = () => {
+//         setAllReviews(reviews.map((review) => review));
+//   }
+
+console.log("R",reviews)
+
+   console.log("ALL", allReviews)
     return(
         <>
         <h3>{title}</h3>
         <p>Description: {event_description}</p>
         <p> Cost: ${price}</p>
-        {/* {allReviewsMap} */}
+        <h4>Reviews</h4>
+        {allReviewsMap}
+        <hr/>
        </> 
     )
     
--- client/src/ReviewTile.js
+++ client/src/ReviewTile.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 
 function ReviewTile({review}){
-   const {title} = review
+   const {title, event_description} = review
     return(
         <>
         <h3>{title}</h3>
+        <p>Description: {event_description}</p>
         </>
     )
     
