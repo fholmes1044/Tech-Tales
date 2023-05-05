aefb3c9fbbfdef215c40a1d11aff4bc13d837c73
+ <+>
2023-05-04 23:06:08 -0400

    user can review events that they have reviewed

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :event_id, :recommend, :summary
+  attributes :id, :user_id, :event_id, :summary
 
   belongs_to :event
   belongs_to :user
--- client/src/ReviewTile.js
+++ client/src/ReviewTile.js
@@ -1,11 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import { UserContext } from "./context/user";
+
+
 
 function ReviewTile({review}){
    const {title, event_description} = review
+   const {user} = useContext(UserContext)
+   console.log("R", review)
+
+   const allReviewsMap = user.reviews.map((review) =>{
+    return(
+        <li key={review.id}>{review.summary}</li>
+    )
+   })
     return(
         <>
         <h3>{title}</h3>
         <p>Description: {event_description}</p>
+        {/* {user.reviews.length > 0 ? {allReviewsMap} : "No reviews yet" } */}
+        <h5>Reviews</h5>
+        {allReviewsMap}
         </>
     )
     
