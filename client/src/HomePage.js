30cb2312fce2bf1af9eaa5ba7d9f325a6d7e4120
+ <+>
2023-09-24 23:52:05 -0400

    added logo and styling to homepage and navbar

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,13 +1,58 @@
 import React, { useContext } from "react";
 import { UserContext} from "./context/user";
+import { Stack, Text,} from "@fluentui/react";
 import "./styling/Home.css"
 
 function HomePage(){
     const { user, loggedIn } = useContext(UserContext)
+    const columnStyle = {
+        flex: '1', 
+        minWidth: '300px', 
+        padding: '16px',
+      };
 
     if (loggedIn === false){
         return (
-            <h3>Please Login or Signup</h3>
+            <>
+            <h3>Tech Tales: Where Every Event Tells a Story.</h3>
+            <div>
+                <img
+                src="https://i.imgur.com/NvMO05y.png"
+                />
+            </div>
+            <Stack
+                horizontal
+                className="homepagetile"
+                tokens={{ childrenGap: 20}}
+                styles={{ 
+                    root: {        
+                        padding: 16,
+                        border: "1px solid #ccc",
+                        borderRadius: 4,
+                        margin: 20,
+                    } 
+                }} 
+            >
+            <Stack.Item>
+            <div style={{ padding: '16px' }}>
+            <Text variant="xxLarge"> Discover </Text>
+            <Text variant="medium">Search Through an Impactful Array of Technology-Centered Events! </Text>
+            </div>
+            </Stack.Item>
+            <Stack.Item>
+            <div style={{ padding: '16px' }}>
+            <Text variant="xxLarge"> Attend</Text>
+            <Text variant="medium">Show Up to Tech Events and Supercharge Your Knowledge to Grow by Leaps and Bounds! </Text>
+            </div>
+            </Stack.Item>
+            <Stack.Item>
+            <div style={{ padding: '16px' }}>
+            <Text variant="xxLarge">Reflect</Text>
+            <Text variat="medium">Reminisce and Preserve through Intentional Reflections to Eternalize Your Tech Event Learning Gems!</Text>
+            </div>
+            </Stack.Item>
+            </Stack>
+            </>
         )
     }
     else {
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -19,60 +19,99 @@ const logoutUser = (e) => {
     })
 }
 
-  if (loggedIn && user) {
+
+if (loggedIn && user) {
   return (
-    <div id="navbar">
+    <nav id="navbar">
+    {/* <div id="navbar"> */}
+      <div className="navbar-container">
+        <div className="navbar-brand">
 
-      <h2 id="navbarh2"> {user.full_name}, It is time to reflect</h2>
-
-      <br/>
-      
+      {/* <h2 id="navbarh2"> {user.full_name}, It is time to reflect</h2> */}
+        <img 
+            id="navbar-logo"
+            src="https://i.imgur.com/NvMO05y.png"
+            alt="Logo"
+          />
+        </div>
+      {/* <br/> */}
+      <ul className="navbar-links">
+        <li className="navbar-item">
       <NavLink
         to="/"
         exact
         activeClassName="activeLink"
-        className="navbar-link"
-        
+        className="navbar-link" 
       >
         Home
       </NavLink>
-     
+        </li>
+        <li className="navbar-item">
       <NavLink
         to="/events"
         exact
         activeClassName="activeLink"
         className="navbar-link"
-      
       >
         All Events
       </NavLink>
-
+        </li>
+        <li className="navbar-item">
       <NavLink
         to="/reviews"
         exact
         activeClassName="activeLink"
         className="navbar-link"
-        
       >
         My Reviewed Events
       </NavLink>
+      </li>
+      <li className="navbar-item">
       <button onClick={logoutUser}>Sign Out</button>
+      </li>
+      </ul>
     </div>
+    </nav>
   );
 }else {
   return (
-    <div>
+    <nav id="navbar">
+     <div className="navbar-container">
+     <ul className="navbar-links">
+     <li className="navbar-item">
       <NavLink
-      to="/login">
-        <button>Login</button>
+        to="/"
+        exact
+        activeClassName="activeLink"
+        className="navbar-link"
+      >
+        <button className="navbar-button">Home</button>
       </NavLink>
+      </li>
+      <li className="navbar-item">
       <NavLink
-        to="/signup">
-        <button>Signup</button>
+        to="/login"
+        activeClassName="activeLink"
+        className="navbar-link"
+      >
+        <button className="navbar-button">Login</button>
       </NavLink>
+      </li>
+      <li className="navbar-item">
+      <NavLink
+        to="/signup"
+        activeClassName="activeLink"
+        className="navbar-link"
+        >
+        <button className="navbar-button">Signup</button>
+      </NavLink>
+      </li>
+    </ul>
     </div>
+    </nav>
   )
 }
+  
   
 }
 
